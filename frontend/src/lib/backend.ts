1324a7d05e2ce358594eebb73c72c59411ff1f8e
Lukas <wicke.lukas@gmail.com>
2025-02-07 04:57:14 +0100

    build fix attempt

--- frontend/src/lib/backend.ts
+++ frontend/src/lib/backend.ts
@@ -2,14 +2,16 @@ import { bindMethods } from "./util/js";
 
 export function useBackend(platform: App.Platform | undefined) {
   const dev = import.meta.env.DEV;
+  const baseUrl = dev ? "http://localhost:8787" : "https://api.kilo.zone";
   if (!platform) throw new Error("Platform is required");
   return bindMethods({
     async getPageData(path: string) {
+      const url = new URL(`${baseUrl}/page/${path}`);
       if (dev) {
-        const res = await fetch(`http://localhost:8787/page/${path}`);
+        const res = await fetch(url);
         return res.ok ? await res.json() : null;
       } else {
-        const res = await platform.env.BACKEND.fetch(`/page/${path}`);
+        const res = await platform.env.BACKEND.fetch(url);
         return res.ok ? await res.json() : null;
       }
     },

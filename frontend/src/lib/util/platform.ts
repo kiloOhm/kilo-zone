c5c89616e2f5367d43a22ca4d5c154bb17d1557e
Lukas <wicke.lukas@gmail.com>
2025-02-07 03:34:04 +0100

    build fix attempt

--- frontend/src/lib/util/platform.ts
+++ frontend/src/lib/util/platform.ts
@@ -2,21 +2,29 @@ export function usePlatform(platform: Readonly<App.Platform> | undefined) {
   if (!platform) {
     throw new Error("Platform is not defined");
   }
+  console.log("Platform", platform);
+  console.log({
+    globalThisFetch: globalThis.fetch,
+    platformFetch: platform.env.BACKEND?.fetch,
+  });
   return {
     env: {
       BACKEND: {
         fetch: import.meta.env.DEV
           ? async (input: RequestInfo, init?: RequestInit) => {
               if (typeof input === "string") {
-                return await globalThis.fetch("http://localhost:8787" + input);
+                return await globalThis.fetch("http://localhost:8787" + input, init);
               } else if (input instanceof Request) {
-                return await globalThis.fetch({
-                  ...input,
-                  url: "http://localhost:8787" + input.url,
-                });
+                return await globalThis.fetch(
+                  {
+                    ...input,
+                    url: "http://localhost:8787" + input.url,
+                  },
+                  init
+                );
               }
             }
-          : platform.env.BACKEND.fetch,
+          : platform.env.BACKEND?.fetch,
       },
     },
   };

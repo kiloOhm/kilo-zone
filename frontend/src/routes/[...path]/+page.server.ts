2bb801a418a9871c81f096eaad983e4bb2606809
Lukas <wicke.lukas@gmail.com>
2025-02-07 03:45:30 +0100

    build fix attempt

--- frontend/src/routes/[...path]/+page.server.ts
+++ frontend/src/routes/[...path]/+page.server.ts
@@ -2,11 +2,9 @@ import { usePlatform } from "$lib";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params, platform }) => {
-  const {
-    env: { BACKEND },
-  } = usePlatform(platform);
+  const p = usePlatform(platform);
   let pageData;
-  const res = await BACKEND.fetch("/page/" + params.path);
+  const res = await p.env.BACKEND.fetch("/page/" + params.path);
   if (!res || !res.ok) {
     console.error("Failed to load page data", res);
   } else {
